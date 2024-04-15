8f10edd1b79485260ba49ff2aaeb9f0301a7c935
mayllon <87451748+m3mayllon@users.noreply.github.com>
2024-04-14 23:02:11 -0700

    fix: backend ports

--- server/djangoapp/microservices/app.py
+++ server/djangoapp/microservices/app.py
@@ -1,35 +1,35 @@
 from flask import Flask
 from nltk.sentiment import SentimentIntensityAnalyzer
 import json
-app = Flask("Sentiment Analyzer")
+app = Flask('Sentiment Analyzer')
 
 sia = SentimentIntensityAnalyzer()
 
 
 @app.get('/')
 def home():
-    return "Welcome to the Sentiment Analyzer. \
-    Use /analyze/text to get the sentiment"
+    return 'Welcome to the Sentiment Analyzer. \
+    Use /analyze/text to get the sentiment'
 
 
 @app.get('/analyze/<input_txt>')
 def analyze_sentiment(input_txt):
 
     scores = sia.polarity_scores(input_txt)
-    print(scores)
+
     pos = float(scores['pos'])
     neg = float(scores['neg'])
     neu = float(scores['neu'])
-    res = "positive"
-    print("pos neg nue ", pos, neg, neu)
+    res = 'positive'
+
     if (neg > pos and neg > neu):
-        res = "negative"
+        res = 'negative'
+
     elif (neu > neg and neu > pos):
-        res = "neutral"
-    res = json.dumps({"sentiment": res})
-    print(res)
-    return res
+        res = 'neutral'
+
+    return json.dumps({'sentiment': res})
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(port=5050, debug=True)
